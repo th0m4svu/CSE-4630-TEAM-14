4508c3a567a5118d7171b8cdd39aed9357aadcd0
thanhrt <63371727+thanhrt@users.noreply.github.com>
2021-11-02 19:04:54 -0500

    Add files via upload

---
+++ proj1final.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+
+#define MAX_OBSTACLES 25 /* maximum number of obstacles */
+int num_obstacles = 13; /* number of obstacles */
+double obstacle[MAX_OBSTACLES][2] = /* obstacle locations */
+{{0.61, 2.743},{0.915, 2.743},{1.219, 2.743},{1.829, 1.219},
+{1.829, 1.524},{ 1.829, 1.829}, {1.829, 2.134},{2.743, 0.305},
+{2.743, 0.61},{2.743, 0.915},{2.743, 2.743},{3.048, 2.743},
+{3.353, 2.743},
+{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},
+{-1,-1},{-1,-1},{-1,-1}};
+double start[2] = {0.305, 1.219}; /* start location */
+double goal[2] = {3.658, 1.829}; /* goal location */
+double tile_length = 0.305; /* length of tile */
+int manhattan_grid[13][19]; /* dimensions for grid */
+int grid_width = sizeof(manhattan_grid)/sizeof(manhattan_grid[0]); //10
+int grid_length = sizeof(manhattan_grid[0])/sizeof(manhattan_grid[0][0]); //16
+
+void grid_setup()
+{
+    // Set up grid
+    int y, x;
+    for (y = 0; y < grid_width; y++) {
+        for (x = 0; x < grid_length; x++) {
+            manhattan_grid[y][x] = -1;
+            //Build the wall
+            if (x == 0 || x == grid_length - 1)
+                    manhattan_grid[y][x] = 99;
+            if (y == 0 || y == grid_width - 1)
+                    manhattan_grid[y][x] = 99;
+        }
+    }
+    //printf("Grid set up to -1.\n");
+
+    // Set up start and goal
+    int x_start = (int) ceil(start[0]/tile_length);
+    int y_start = (int) ceil(start[1]/tile_length);
+    //manhattan_grid[y_start][x_start] = 98;
+    manhattan_grid[(grid_width-y_start)-2][x_start+1] = 98;
+   
+    int x_goal = (int) ceil(goal[0]/tile_length);
+    int y_goal = (int) ceil(goal[1]/tile_length);
+    manhattan_grid[(grid_width-y_goal)-2][x_goal+1] = 0;
+    printf("Start and goal set up.\n");
+
+    // Set up obstacles
+    int x_obs = 0;
+    int y_obs = 0;
+    int k, j;
+    for (k = 0; k < num_obstacles; k++) {
+        x_obs = (int) ceil(obstacle[k][0]/tile_length);
+        y_obs = (int) ceil(obstacle[k][1]/tile_length);
+        manhattan_grid[(grid_width-y_obs)-2][x_obs+1] = 99;
+    }
+    printf("Obstacles set up.\n");
+}
+
+void print_grid() 
+{
+    int y, x;
+    for (y = 0; y < grid_width; y++) {
+        for (x = 0; x < grid_length; x++) {
+            printf("%3d ", manhattan_grid[y][x]);
+        }
+        printf("\n");
+    }
+}
+
+void propagation()
+{
+    int current = 0;
+    int y, x;
+    while(current<(grid_length*grid_width))
+    {
+        for (y = 0; y < grid_width; y++) 
+        {
+            for (x = 0; x < grid_length; x++) 
+            {
+                if(manhattan_grid[y][x]==current)
+                {
+                    if(manhattan_grid[y+1][x]==-1)
+                        manhattan_grid[y+1][x]=current+1;
+                    if(manhattan_grid[y-1][x]==-1)
+                        manhattan_grid[y-1][x]=current+1;
+                    if(manhattan_grid[y][x-1]==-1)
+                        manhattan_grid[y][x-1]=current+1;
+                    if(manhattan_grid[y][x+1]==-1)
+                        manhattan_grid[y][x+1]=current+1; 
+                }
+            }
+            printf("\n");
+        }
+        current++;
+    }
+}
+
+void pathfinding()
+{
+    int x_start = (int) ceil(start[0]/tile_length);
+    int y_start = (int) ceil(start[1]/tile_length);
+
+    int x = (grid_width-y_start)-2;
+    int y = x_start+1;
+
+    int top, right, bottom, left;
+    int curr_pos[2] = {x,y};
+    int currentvalue = 98;
+    while(currentvalue!=0)
+    {
+        x = curr_pos[0];
+        y = curr_pos[1];
+        top = manhattan_grid[x-1][y];
+        right = manhattan_grid[x][y+1];
+        bottom = manhattan_grid[x+1][y];
+        left = manhattan_grid[x][y-1];
+
+        if(top<=bottom && top<=right &&  top<=left) //Going up
+        {
+            curr_pos[0] = x-1;
+            curr_pos[1] = y;
+            currentvalue = manhattan_grid[x-1][y];
+            manhattan_grid[x-1][y] = 98;
+        }
+        else if(right<=top  && right<=left && right<=bottom) //Going right
+        {
+            curr_pos[0] = x;
+            curr_pos[1] = y+1;
+            currentvalue = manhattan_grid[x][y+1];
+            manhattan_grid[x][y+1] = 98;
+        }
+        else if(bottom<=top  && bottom<=right && bottom<=left) //Going down
+        {
+            curr_pos[0] = x+1;
+            curr_pos[1] = y;
+            currentvalue = manhattan_grid[x+1][y];
+            manhattan_grid[x+1][y] = 98;
+        }
+        else if(left<=top && left<=right && left<=bottom) //Going left
+        {
+            curr_pos[0] = x;
+            curr_pos[1] = y-1;
+            currentvalue = manhattan_grid[x][y-1];
+            manhattan_grid[x][y-1] = 98;
+        }
+    }
+}
+
+int main() 
+{
+    grid_setup();
+    propagation();
+    pathfinding();
+    print_grid();
+    return 0;
+}
